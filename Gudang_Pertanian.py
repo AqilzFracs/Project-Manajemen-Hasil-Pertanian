ade9554e14c64b14eae7a11bd843660abafe85fc
AqilzFracs <mahdiagilali@gmail.com>
2023-12-13 21:28:18 +0700

    add Delete and Edit

--- Gudang_Pertanian.py
+++ Gudang_Pertanian.py
@@ -3,7 +3,7 @@ import csv
 
 class RedBlackTreeNode:
     def __init__(self, nama, jumlah, color, left=None, right=None, parent=None):
-        self.nama = nama  # Nomor telepon sebagai kunci
+        self.nama = nama
         self.jumlah = jumlah
         self.color = color
         self.left = left
@@ -13,7 +13,7 @@ class RedBlackTreeNode:
 class RedBlackTree:
     def __init__(self, data_file="hasilPertanian.csv"):
         self.NIL = RedBlackTreeNode(None, None, 0)
-        self.root = self.NIL
+        self.root = self.NIL             
         self.load_from_file(data_file)
 
     def insert(self, nama, jumlah):
@@ -49,10 +49,10 @@ class RedBlackTree:
                 else:
                     if node == node.parent.right:
                         node = node.parent
-                        self._left_rotate(node)
+                        self.left_rotate(node)
                     node.parent.color = 0
                     node.parent.parent.color = 1
-                    self._right_rotate(node.parent.parent)
+                    self.right_rotate(node.parent.parent)
             else:
                 y = node.parent.parent.left
                 if y.color == 1:
@@ -63,13 +63,13 @@ class RedBlackTree:
                 else:
                     if node == node.parent.left:
                         node = node.parent
-                        self._right_rotate(node)
+                        self.right_rotate(node)
                     node.parent.color = 0
                     node.parent.parent.color = 1
-                    self._left_rotate(node.parent.parent)
+                    self.left_rotate(node.parent.parent)
         self.root.color = 0
 
-    def _left_rotate(self, x):
+    def left_rotate(self, x):
         y = x.right
         x.right = y.left
         if y.left != self.NIL:
@@ -84,56 +84,56 @@ class RedBlackTree:
         y.left = x
         x.parent = y
 
-    def _right_rotate(self, y):
-        x = y.left
-        y.left = x.right
-        if x.right != self.NIL:
-            x.right.parent = y
-        x.parent = y.parent
-        if y.parent == self.NIL:
-            self.root = x
-        elif y == y.parent.left:
-            y.parent.left = x
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.NIL:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent == self.NIL:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
         else:
-            y.parent.right = x
-        x.right = y
-        y.parent = x
+            x.parent.right = y
+        y.right = x
+        x.parent = y
 
     def search(self, criteria, value):
         return self.search_helper(self.root, criteria, value)
   
     def search_helper(self, node, criteria, value):
-        while node != self.NIL:
-            if criteria == "nama":
-                if value.lower() == node.nama.lower():
-                    return node
-                elif value.lower() < node.nama.lower():
-                    node = node.left
-                else:
-                    node = node.right
-            elif criteria == "jumlah":
-                if value == node.jumlah:
-                    return node
-                if value < node.jumlah:
-                    node = node.left
-                else:
-                    node = node.right
-        return self.NIL
-
+        print(f"Current Node: {node.nama}, {node.jumlah}")
+        if criteria == "nama":
+            if value.lower() == node.nama.lower():
+                return node
+            elif value.lower() < node.nama.lower():
+                node = node.left
+            else:
+                node = node.right
+        elif criteria == "jumlah":
+            # node_jumlah = int(node.jumlah)
+            print(f"Comparing {value} with {node.jumlah}")
+            if node == self.NIL or value == node.jumlah:
+                return node
+            if value < node.jumlah:
+                return self.search_helper(node.left, criteria, value)
+            return self.search_helper(node.right, criteria, value)
+                
     def search_and_display(self, criteria, value):
       result = self.search_helper(self.root, criteria, value)
       if result != self.NIL:
-          data = self._inorder_traversal(result)
+          data = self.inorder_traversal(result)
           headers = ["Nama", "Jumlah"]
           print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
       else:
           print("Data tidak ditemukan.")
 
-    def _inorder_traversal(self, node, descending=False):
+    def inorder_traversal(self, node, descending=False):
         if node != self.NIL:
-            left_data = self._inorder_traversal(node.left, descending)
+            left_data = self.inorder_traversal(node.left, descending)
             current_data = [[node.nama, node.jumlah]]
-            right_data = self._inorder_traversal(node.right, descending)
+            right_data = self.inorder_traversal(node.right, descending)
 
             if descending:
                 return right_data + current_data + left_data
@@ -143,44 +143,44 @@ class RedBlackTree:
             return []
 
     def display_data(self, descending=False):
-        data = self._inorder_traversal(self.root, descending)
+        data = self.inorder_traversal(self.root, descending)
         if data:
             print(tabulate(data, headers=["Nama", "Jumlah"], tablefmt="fancy_grid"))
         else:
             print("Tidak ada data data.")
 
-    def delete(self, nama):
-        node = self.search(nama)
+    def delete(self, criteria, nama):
+        node = self.search(criteria, nama)
         if node != self.NIL:
-            self._delete_node(node)
+            self.delete_node(node)
 
-    def _delete_node(self, node):
+    def delete_node(self, node):
         y = node
         y_original_color = y.color
         if node.left == self.NIL:
             x = node.right
-            self._transplant(node, node.right)
+            self.transplant(node, node.right)
         elif node.right == self.NIL:
             x = node.left
-            self._transplant(node, node.left)
+            self.transplant(node, node.left)
         else:
-            y = self._minimum(node.right)
+            y = self.minimum(node.right)
             y_original_color = y.color
             x = y.right
             if y.parent == node:
                 x.parent = y
             else:
-                self._transplant(y, y.right)
+                self.transplant(y, y.right)
                 y.right = node.right
                 y.right.parent = y
-            self._transplant(node, y)
+            self.transplant(node, y)
             y.left = node.left
             y.left.parent = y
             y.color = node.color
         if y_original_color == 0:
-            self._fix_delete(x)
+            self.fix_delete(x)
 
-    def _transplant(self, u, v):
+    def transplant(self, u, v):
         if u.parent == self.NIL:
             self.root = v
         elif u == u.parent.left:
@@ -189,19 +189,19 @@ class RedBlackTree:
             u.parent.right = v
         v.parent = u.parent
 
-    def _minimum(self, node):
+    def minimum(self, node):
         while node.left != self.NIL:
             node = node.left
         return node
 
-    def _fix_delete(self, x):
+    def fix_delete(self, x):
         while x != self.root and x.color == 0:
             if x == x.parent.left:
                 w = x.parent.right
                 if w.color == 1:
                     w.color = 0
                     x.parent.color = 1
-                    self._left_rotate(x.parent)
+                    self.left_rotate(x.parent)
                     w = x.parent.right
                 if w.left.color == 0 and w.right.color == 0:
                     w.color = 1
@@ -210,19 +210,19 @@ class RedBlackTree:
                     if w.right.color == 0:
                         w.left.color = 0
                         w.color = 1
-                        self._right_rotate(w)
+                        self.right_rotate(w)
                         w = x.parent.right
                     w.color = x.parent.color
                     x.parent.color = 0
                     w.right.color = 0
-                    self._left_rotate(x.parent)
+                    self.left_rotate(x.parent)
                     x = self.root
             else:
                 w = x.parent.left
                 if w.color == 1:
                     w.color = 0
                     x.parent.color = 1
-                    self._right_rotate(x.parent)
+                    self.right_rotate(x.parent)
                     w = x.parent.left
                 if w.right.color == 0 and w.left.color == 0:
                     w.color = 1
@@ -231,17 +231,26 @@ class RedBlackTree:
                     if w.left.color == 0:
                         w.right.color = 0
                         w.color = 1
-                        self._left_rotate(w)
+                        self.left_rotate(w)
                         w = x.parent.left
                     w.color = x.parent.color
                     x.parent.color = 0
                     w.left.color = 0
-                    self._right_rotate(x.parent)
+                    self.right_rotate(x.parent)
                     x = self.root
         x.color = 0
 
+    def edit(self, old_nama, new_nama, new_jumlah):
+        node_to_edit = self.search_helper(self.root, "nama", old_nama)
+        if node_to_edit != self.NIL:
+            self.delete_node(node_to_edit)
+            self.insert(new_nama, new_jumlah)
+            print("Data berhasil diperbarui.")
+        else:
+            print("Data tidak ditemukan.")
+
     def save_to_file(self, filename="hasilPertanian.csv"):
-        data = self._inorder_traversal(self.root)
+        data = self.inorder_traversal(self.root)
         with open(filename, 'w', newline='') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow(["Nama", "Jumlah"])  # Menulis header
